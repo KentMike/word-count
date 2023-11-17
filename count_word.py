320406ab2d8bd0af49308a828ddbed6eeac4296f
Kenneth Michael ogwok <ogwok.kent@gmail.com>
2023-11-17 14:37:54 +0300

    -A

--- count_word.py
+++ count_word.py
@@ -1,27 +1,38 @@
 import sys
-
 from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
 
-  # create Spark context with Spark configuration
-  conf = SparkConf().setAppName("SparkWordCount")
-  sc = SparkContext(conf=conf)
+    # Check if the correct number of command line arguments is provided
+    if len(sys.argv) != 3:
+        print("Usage: spark-submit count_word.py C:/Users/user/Wordcount-CW/shakes.txt")
+        sys.exit(1)
 
-  # get threshold
-  threshold = int(sys.argv[2])
+    # create Spark context with Spark configuration
+    conf = SparkConf().setAppName("SparkWordCount")
+    sc = SparkContext(conf=conf)
 
-  # read in text file and split each document into words
-  tokenized = sc.textFile(sys.argv[1]).flatMap(lambda line: line.split(" "))
+    try:
+        # get threshold
+        threshold = int(sys.argv[2])
 
-  # count the occurrence of each word
-  wordCounts = tokenized.map(lambda word: (word, 1)).reduceByKey(lambda v1,v2:v1 +v2)
+        # read in text file and split each document into words
+        tokenized = sc.textFile(sys.argv[1]).flatMap(lambda line: line.split(" "))
 
-  # filter out words with fewer than threshold occurrences
-  filtered = wordCounts.filter(lambda pair:pair[1] >= threshold)
+        # count the occurrence of each word
+        wordCounts = tokenized.map(lambda word: (word, 1)).reduceByKey(lambda v1, v2: v1 + v2)
 
-  # count characters
-  charCounts = filtered.flatMap(lambda pair:pair[0]).map(lambda c: c).map(lambda c: (c, 1)).reduceByKey(lambda v1,v2:v1 +v2)
+        # filter out words with fewer than threshold occurrences
+        filtered = wordCounts.filter(lambda pair: pair[1] >= threshold)
 
-  list = charCounts.collect()
-  print (repr(list)[1:-1])
+        # count characters
+        charCounts = filtered.flatMap(lambda pair: pair[0]).map(lambda c: c).map(lambda c: (c, 1)).reduceByKey(
+            lambda v1, v2: v1 + v2
+        )
+
+        result_list = charCounts.collect()
+        print(repr(result_list)[1:-1])
+
+    finally:
+        # Stop the Spark context
+        sc.stop()
